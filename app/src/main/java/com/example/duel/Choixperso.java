225aa0d40767ca72c65dab7576f7a2bd4a869d65
CodingStrqf <l.ruy42@orange.fr>
2022-02-08 17:24:26 +0100

    réalisation de la vue permettant de choisir le personnage

--- app/src/main/java/com/example/duel/Choixperso.java
+++ app/src/main/java/com/example/duel/Choixperso.java
@@ -3,6 +3,16 @@ package com.example.duel;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.duel.model.Personnage;
+import model.Paladin;
 
 public class Choixperso extends AppCompatActivity {
 
@@ -10,5 +20,57 @@ public class Choixperso extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.choixperso);
+
+        EditText pseudo = findViewById(R.id.pseudo);
+
+        Button paladin = findViewById(R.id.paladin);
+        Button magicien = findViewById(R.id.magicien);
+        Button chimiste = findViewById(R.id.chimiste);
+        Button voleur = findViewById(R.id.voleur);
+        Button confirmation = findViewById(R.id.confirmer);
+
+        pseudo.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                pseudo.getText().clear();
+                return false;
+            }
+        });
+        paladin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ImageView perso = findViewById(R.id.perso);
+                perso.setImageResource(R.drawable.paladin);
+                //création du perso ici pour avoir accès a ses différentes information pour les affichers
+                Personnage joueur = new Paladin(pseudo.getText().toString());
+                Log.d("perso",joueur.toString() );
+            }
+        });
+        magicien.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ImageView perso = findViewById(R.id.perso);
+                perso.setImageResource(R.drawable.mage);
+            }
+        });
+        chimiste.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ImageView perso = findViewById(R.id.perso);
+                perso.setImageResource(R.drawable.chimiste);
+            }
+        });
+        voleur.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ImageView perso = findViewById(R.id.perso);
+                perso.setImageResource(R.drawable.voleur);
+            }
+        });
+        confirmation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
     }
 }
